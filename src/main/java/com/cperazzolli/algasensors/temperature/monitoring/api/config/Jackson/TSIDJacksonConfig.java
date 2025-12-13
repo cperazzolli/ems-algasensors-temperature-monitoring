5f135960e47ae5a857c891ac0dca0fab5023d328
Claudinei Perazzolli <claudineii.perazzolli@gmail.com>
2025-12-12 22:02:13 -0300

    temperature-monitoring initial

---
+++ src/main/java/com/cperazzolli/algasensors/temperature/monitoring/api/config/Jackson/TSIDJacksonConfig.java
@@ -0,0 +1,18 @@
+package com.cperazzolli.algasensors.temperature.monitoring.api.config.Jackson;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import io.hypersistence.tsid.TSID;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TSIDJacksonConfig {
+
+    @Bean
+    public Module jacksonConfig() {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(TSID.class, new TSIDToStringSerializer());
+        return module;
+    }
+}
